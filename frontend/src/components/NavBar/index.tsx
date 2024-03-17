0ffd5591e6c7c841576bc0ab483bf590ef64b47f
stelios <stelioscbranco@hotmail.com>
2024-03-17 16:22:08 -0300

    improving links

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { company_name, background_color_default } from "@/lib/constants";
+import { background_color_default, company_name } from "@/lib/constants";
 import Link from "next/link";
 
 export const NavBar = (): JSX.Element => {
@@ -28,23 +28,23 @@ export const NavBar = (): JSX.Element => {
         display: "inline-flex",
       }}
     >
-      <div
-        style={{
-          width: "138px",
-          alignSelf: "stretch",
-          textAlign: "center",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Inter",
-          fontWeight: 500,
-          lineHeight: "30px",
-          wordWrap: "break-word",
-        }}
-      >
-        <Link href="/">
-        {company_name}
-        </Link>
-      </div>
+      <Link href="/">
+        <div
+          style={{
+            width: "138px",
+            alignSelf: "stretch",
+            textAlign: "center",
+            color: "black",
+            fontSize: "20px",
+            fontFamily: "Inter",
+            fontWeight: 500,
+            lineHeight: "30px",
+            wordWrap: "break-word",
+          }}
+        >
+          {company_name}
+        </div>
+      </Link>
       <div
         style={{
           width: "515px",
@@ -57,18 +57,18 @@ export const NavBar = (): JSX.Element => {
       >
         {links.map((link, index) => (
           <Link key={index} href={link.url}>
-          <div
-            style={{
-              color: "black",
-              fontSize: "15px",
-              fontFamily: "Inter",
-              fontWeight: 400,
-              lineHeight: "22.50px",
-              wordWrap: "break-word",
-            }}
-          >
-            {link.name}
-          </div>
+            <div
+              style={{
+                color: "black",
+                fontSize: "15px",
+                fontFamily: "Inter",
+                fontWeight: 400,
+                lineHeight: "22.50px",
+                wordWrap: "break-word",
+              }}
+            >
+              {link.name}
+            </div>
           </Link>
         ))}
       </div>
@@ -85,29 +85,29 @@ export const NavBar = (): JSX.Element => {
       >
         {buttons.map((button, index) => (
           <Link key={index} href={button.url}>
-          <div
-            style={{
-              paddingLeft: "16px",
-              paddingRight: "16px",
-              paddingTop: "14px",
-              paddingBottom: "14px",
-              background: "#70CBFF",
-              boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.05)",
-              borderRadius: "16px",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "16px",
-              display: "flex",
-              color: "white",
-              fontSize: "16px",
-              fontFamily: "Inter",
-              fontWeight: 500,
-              lineHeight: "24px",
-              wordWrap: "break-word",
-            }}
-          >
-            {button.name}
-          </div>
+            <div
+              style={{
+                paddingLeft: "16px",
+                paddingRight: "16px",
+                paddingTop: "14px",
+                paddingBottom: "14px",
+                background: "#70CBFF",
+                boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.05)",
+                borderRadius: "16px",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "16px",
+                display: "flex",
+                color: "white",
+                fontSize: "16px",
+                fontFamily: "Inter",
+                fontWeight: 500,
+                lineHeight: "24px",
+                wordWrap: "break-word",
+              }}
+            >
+              {button.name}
+            </div>
           </Link>
         ))}
       </div>
