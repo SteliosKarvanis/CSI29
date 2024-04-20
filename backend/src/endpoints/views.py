cb347895c491b68ff98a0818933909a58bff18a0
danielquintao <danielquintaodemoraes@gmail.com>
2024-04-20 20:31:44 -0300

    first endpoint

--- backend/src/endpoints/views.py
+++ backend/src/endpoints/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.db.models import Q
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Corretor, Imovel, Casa, Apartamento, Endereco, Multimidia, Proprietario
 
-# Create your views here.
+def imoveis_ativos(request):
+    """Retorna informação dos imíveis disponíveis para compra ou aluguel."""
+
+    imoveis_ativos = Imovel.objects.filter(Q(comprar__exact=True) | Q(alugar__exact=True))
+    data = serializers.serialize("json", imoveis_ativos)
+    return JsonResponse(data, safe=False)
