e79869d1148391ffb9c3763074f24037b6fd7b9c
danielquintao <danielquintaodemoraes@gmail.com>
2024-04-20 20:37:52 -0300

    fix imoveis_Ativos response from string to json

--- backend/src/endpoints/views.py
+++ backend/src/endpoints/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import HttpResponse
 from django.core import serializers
 from .models import Corretor, Imovel, Casa, Apartamento, Endereco, Multimidia, Proprietario
 
@@ -9,4 +9,4 @@ def imoveis_ativos(request):
 
     imoveis_ativos = Imovel.objects.filter(Q(comprar__exact=True) | Q(alugar__exact=True))
     data = serializers.serialize("json", imoveis_ativos)
-    return JsonResponse(data, safe=False)
+    return HttpResponse(data, content_type='application/json')
