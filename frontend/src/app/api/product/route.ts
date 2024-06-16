553a58f436d9604cb9178bc1a46880ad30f66d22
danielquintao <danielquintaodemoraes@gmail.com>
2024-06-16 16:10:19 -0300

    fix host unreachable

--- frontend/src/app/api/product/route.ts
+++ frontend/src/app/api/product/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch('http://localhost:8000/endpoints/imoveis_ativos_info_completa', {
+    const res = await fetch('http://backend:8000/endpoints/imoveis_ativos_info_completa', {
       headers: {
         'Content-Type': 'application/json'
       },
