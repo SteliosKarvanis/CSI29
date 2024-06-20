c6eb46c39b37c623115f6fbb40db39f869ddf61a
stelios <stelioscbranco@hotmail.com>
2024-06-19 23:07:37 -0300

    wip

--- frontend/src/app/api/product/route.ts
+++ frontend/src/app/api/product/route.ts
@@ -1,10 +1,10 @@
-export async function GET() {
-    const res = await fetch('http://backend:8000/endpoints/imoveis_ativos_info_completa', {
+export async function GET(url: string) {
+    const res = await fetch(url, {
       headers: {
         'Content-Type': 'application/json'
       },
     })
     const data = await res.json()
    
-    return Response.json({ data })
+    return data
   }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,37 +3,39 @@ import { Search } from "@/components/Search";
 import { VerticalCardList } from "@/components/VerticalCardList";
 import { VerticalCardListProps } from "@/lib/types";
 import { Col } from "antd";
+import { GET } from "./api/product/route";
 
-export default function Home() {
+export default async function Home() {
+  const responses = await GET('http://backend:8000/endpoints/imoveis_ativos_info_completa');
   const cardsList: VerticalCardListProps = {
     name: "Em Alta",
     cards: [
       {
-        id: "1",
-        city: "São Paulo",
-        county: "São Paulo",
-        street: "Rua das Flores, 123",
-        price: 500000,
-        pricePerSquareMeter: 5000,
-        imgSrc: "/house_example.jpg",
+        id: "0",
+        city: responses[0]["nome_residencia"],
+        county: responses[0]["nome_residencia"],
+        street: responses[0]["descricao"],
+        price: responses[0]["preco_compra"],
+        pricePerSquareMeter: responses[0]["metros_quadrados"],
+        imgSrc: "/house_example.jpg" //"http://backend:8000/static/" + responses[0]["images"][0], // TODO: image not found
       },
       {
         id: "1",
-        city: "São Paulo",
-        county: "São Paulo",
-        street: "Rua das Flores, 123",
-        price: 500000,
-        pricePerSquareMeter: 5000,
-        imgSrc: "/house_example.jpg",
+        city: responses[1]["nome_residencia"],
+        county: responses[1]["nome_residencia"],
+        street: responses[1]["descricao"],
+        price: responses[1]["preco_compra"],
+        pricePerSquareMeter: responses[1]["metros_quadrados"],
+        imgSrc: "/house_example.jpg"// "http://backend:8000/static/" + responses[1]["images"][0], // TODO: image not found
       },
       {
-        id: "1",
-        city: "São Paulo",
-        county: "São Paulo",
-        street: "Rua das Flores, 123",
-        price: 500000,
-        pricePerSquareMeter: 5000,
-        imgSrc: "/house_example.jpg",
+        id: "2",
+        city: responses[2]["nome_residencia"],
+        county: responses[2]["nome_residencia"],
+        street: responses[2]["descricao"],
+        price: responses[2]["preco_compra"],
+        pricePerSquareMeter: responses[2]["metros_quadrados"],
+        imgSrc: "/house_example.jpg" //"http://backend:8000/static/" + responses[2]["images"][0], // TODO: image not found
       },
     ],
   };
