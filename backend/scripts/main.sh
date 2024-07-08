9a1107da7ed0d074d6abba970e49502ee9b4157d
danielquintao <danielquintaodemoraes@gmail.com>
2024-07-08 20:55:54 -0300

    add unit test

--- backend/scripts/main.sh
+++ backend/scripts/main.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-../scripts/start_db.sh
-python manage.py shell < ../scripts/create_admin.py
-python manage.py runserver 0.0.0.0:8000
+ls
+/usr/app/scripts/start_db.sh
+/usr/app/scripts/run_test.sh
+python src/manage.py shell < /usr/app/scripts/create_admin.py
+python src/manage.py runserver 0.0.0.0:8000
