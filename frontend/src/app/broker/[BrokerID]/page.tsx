e65b2080b8984d3b3c9734d3a3667927d21abee6
stelios <stelioscbranco@hotmail.com>
2024-07-09 00:18:13 -0300

    wip

--- frontend/src/app/broker/[BrokerID]/page.tsx
+++ frontend/src/app/broker/[BrokerID]/page.tsx
@@ -4,17 +4,16 @@ import { GET_HORIZONTAL_CARD_LIST } from "../../api/product/route";
 
 export default async function Broker() {
   // const responses = await GET_HORIZONTAL_CARD_LIST('http://backend:8000/endpoints/imoveis_por_proprietario/' + brokerID);
-  const responses = await GET_HORIZONTAL_CARD_LIST('http://backend:8000/endpoints/imoveis_ativos_info_completa');
+  const responses = await GET_HORIZONTAL_CARD_LIST(
+    "http://backend:8000/endpoints/imoveis_ativos_info_completa"
+  );
   const props: HorizontalCardListProps = {
     name: "My Ads",
-    cards: [],
+    cards: responses,
   };
-  for (const response of responses) {
-    props.cards.push(response);
-  }
   return (
     <div>
       <HorizontalCardList isBrokerPage {...props} />;
     </div>
   );
-};
+}
